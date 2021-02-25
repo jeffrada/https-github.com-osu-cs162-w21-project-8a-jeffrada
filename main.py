4ba6567d483d82a5e96bbb1d3546212b16d3592a
Adam Jeffries <ajlc2012@yahoo.com>
2021-02-24 22:55:43 -0500

    Commit 1

---
+++ main.py
@@ -0,0 +1,27 @@
+# Author: Adam Jeffries
+# Date: 2/24/2021
+# Description: A generator function named count_seq that doesn't take any parameters and generates a given sequence.
+
+def count_seq():
+    current_step = '2'
+    while True:
+        yield current_step
+        number = '0'
+        count = 0
+        next_step = ''
+        for num in current_step:
+            if num != number:
+                if count != 0:
+                    next_step += str(count)
+                    next_step += number
+                number = num
+                count = 1
+            else:
+                count = count + 1
+        next_step += str(count)
+        next_step += number
+        current_step = next_step
+
+# Testing
+for i in count_seq():
+    print(i)
